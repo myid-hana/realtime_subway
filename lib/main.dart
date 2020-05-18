0913e30a2b744a813cd3fbb2983728e628ad82b4
myid-hana <58727114+myid-hana@users.noreply.github.com>
2020-05-18 11:20:25 +0900

    api 정보 가져오기
    인증키를 기준으로 앞뒤로 api 정보를 나눠서 변수에 저장

--- lib/main.dart
+++ lib/main.dart
@@ -18,11 +18,23 @@ class MainPage extends StatefulWidget {
 }
 
 class _MainPageState extends State<MainPage> {
+
+  //API 정보
+  static const String _urlPrefix = 'http://swopenapi.seoul.go.kr/api/subway/';  //인증키 앞 주소
+  static const String _userkey = 'sample';  //인증키
+  static const String _urlsuffix = '/json/realtimeStationArrival/0/5/';  //인증키 뒷 주소
+  static const String _defaultStation = '신도림';   //받아올 변수값(지하철 명)
+
+  
+
+
   @override
   Widget build(BuildContext context) {
     return Container();
   }
 }
+
+//http://swopenapi.seoul.go.kr/api/subway/sample/json/realtimeStationArrival/0/5/%EC%8B%A0%EB%8F%84%EB%A6%BC
 
 
 
