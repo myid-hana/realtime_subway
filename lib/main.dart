fd6614ddaafa88bf4a4cf3c1dc52b165b6418845
myid-hana <58727114+myid-hana@users.noreply.github.com>
2020-05-18 11:13:41 +0900

    RealTimeSubway main 함수 생성
    main: RealTimeSubway StatelessWidget
    home: MainPage  StatefulWidget

--- lib/main.dart
+++ lib/main.dart
@@ -1,12 +1,30 @@
 import 'package:flutter/material.dart';
 import 'package:http/http.dart' as http;
 
-void main() async {  //비동기식
-  String url = 'http://jsonplaceholder.typicode.com/todos/1';  //json 주소
-  var response = await http.get(url);   //응답
-  print(response.statusCode);  //상태 코드 : 200 정상
-  print(response.body);  
+void main() => runApp(RealTimeSubway());
+
+class RealTimeSubway extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      home: MainPage(),
+    );
+  }
 }
+
+class MainPage extends StatefulWidget {
+  @override
+  _MainPageState createState() => _MainPageState();
+}
+
+class _MainPageState extends State<MainPage> {
+  @override
+  Widget build(BuildContext context) {
+    return Container();
+  }
+}
+
+
 
 
 
