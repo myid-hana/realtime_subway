9771d16ad994729adcfeb923c7bcf6075b49fcf3
myid-hana <58727114+myid-hana@users.noreply.github.com>
2020-05-18 11:10:04 +0900

    연습용 json 데이터 가져오기
    main 함수 생성

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,12 @@
 import 'package:flutter/material.dart';
 import 'package:http/http.dart' as http;
 
+void main() async {  //비동기식
+  String url = 'http://jsonplaceholder.typicode.com/todos/1';  //json 주소
+  var response = await http.get(url);   //응답
+  print(response.statusCode);  //상태 코드 : 200 정상
+  print(response.body);  
+}
+
 
 
